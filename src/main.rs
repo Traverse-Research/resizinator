4608bad11a42efcfa28ce524797a47f014ccc0b0
Max Oomen <max@traverseresearch.nl>
2021-09-01 14:49:15 +0200

    Make window repositioning optional.

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ fn main() {
     let resolution_max_x = 1920;
     let resolution_max_y = 1080;
     let dpi_scale = 1.5;
+    let apply_random_position = false;
 
 
     let mut rng = rand::thread_rng();
@@ -14,12 +15,18 @@ fn main() {
     let max_y = (resolution_max_y as f32 * dpi_scale) as usize;
     loop {
 
-
-        // Use wmctrl to move the window to a random spot with a random size.
-        let x = rng.gen_range::<usize, _>(0..max_x);
-        let y = rng.gen_range::<usize, _>(0..max_y);
+        // Generate a random position and random size.
+        let (x, y) = if apply_random_position {
+            let x = rng.gen_range::<usize, _>(0..max_x);
+            let y = rng.gen_range::<usize, _>(0..max_y);
+            (x, y)
+        } else {
+            (0, 0)
+        };
         let width = rng.gen_range::<usize, _>(0..max_x);
         let height = rng.gen_range::<usize, _>(0..max_y);
+
+        // Use wmctrl to move and resize the window.
         let args = format!("0,{},{},{},{}", x, y, width, height);
         Command::new("sh")
             .arg("-c")
@@ -27,7 +34,7 @@ fn main() {
             .output()
             .expect("failed to execute process");
 
-        // Wait a random amount of time
+        // Wait a random amount of time.
         let delay_max = 150;
         let delay = rng.gen_range::<usize, _>(1..delay_max);
         // Curve the delay so we get more short delays than long delays
